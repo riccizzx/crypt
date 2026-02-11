000245fd4d56ba2c4bbd3c723a7899cd682be9f5
angel <guilhermericci1445@gmail.com>
2026-02-11 10:54:29 -0300

    4challange.txt - file add

--- crypt/main.cpp
+++ crypt/main.cpp
@@ -5,23 +5,33 @@
 
 int main() {
 
+	std::cout << "this program is a collection of solutions to the cryptopals challenges. The challenges are organized in 8 sets,";
+	std::cout << "each set contains 8 challenges.";
+	std::cout << "The challenges are designed to be solved in order, but you can skip around if you want.\n\n";
+
 	usage();
 
 	char choice;
 	std::cin >> choice;
 
 	switch (choice) {
-	
+
 		case '1':
 			crypt::hex_to_base64();
 			break;
+
 		case '2':
 			crypt::fixed_xor();
 			break;
+
 		case '3':
 			crypt::xor_cipher();
 			break;
 
+		case '4':
+			crypt::single_char_xor();
+			break;
+			
 		default:
 			std::cout << "invalid choice\n";
 	
